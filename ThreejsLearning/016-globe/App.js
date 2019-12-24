13f960ef322f6c33eb0985f7499233a701bf6b0a
lianming <dushi@dushideMacBook-Air.local>
2019-12-24 18:40:38 +0800

    es6 重构地球经纬

--- ThreejsLearning/016-globe/App.js
+++ ThreejsLearning/016-globe/App.js
@@ -1,80 +1,86 @@
 import { fragmentShader } from './fragmentShader.glsl.js'
 import { vertexShader } from './vertexShader.glsl.js'
 
-var container;
-var camera, scene, renderer;
-var uniforms;
+class App {
+  constructor() {
+    this.container = ""
+    this.camera = null
+    this.scene = null
+    this.renderer = null
+    this.uniforms = null
+    this.onWindowResize = this.onWindowResize.bind(this);
+    this.animate = this.animate.bind(this);
+    this.render = this.render.bind(this);
 
-function loadTexture(url) {
-  return new Promise((resolve, reject) => {
-    const loader = new THREE.TextureLoader();
-    loader.load(url, (res) => {
-      resolve(res)
-    });
-  })
-}
-
-async function init() {
-  container = document.getElementById('container');
-  camera = new THREE.Camera();
-  camera.position.z = 1;
-  scene = new THREE.Scene();
-  window.scene = scene
-  scene.add(camera)
-  const texture = await loadTexture("./uv.png")
-  texture.minFilter = THREE.NearestFilter;
-  texture.magFilter = THREE.NearestFilter;
-  texture.wrapS = THREE.RepeatWrapping;
-  texture.wrapT = THREE.RepeatWrapping;
-  const plane = new THREE.PlaneBufferGeometry(2, 2);
-  uniforms = {
-    u_time: { type: "f", value: 0 },
-    u_resolution: { type: "v3", value: new THREE.Vector3() },
-    u_mouse: { type: "v2", value: new THREE.Vector2() },
-    iChannel0: { type: "t", value: texture }
-  };
-
-  var material = new THREE.ShaderMaterial({
-    uniforms: uniforms,
-    vertexShader: vertexShader,//document.getElementById('vertexShader').textContent,
-    fragmentShader: fragmentShader//document.getElementById('fragmentShader').textContent
-  });
-
-  var mesh = new THREE.Mesh(plane, material);
-  scene.add(mesh);
-
-  renderer = new THREE.WebGLRenderer();
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.autoClearColor = false;
-  container.appendChild(renderer.domElement);
-
-  onWindowResize();
-  window.addEventListener('resize', onWindowResize, false);
-
-  document.onmousemove = function (e) {
-    uniforms.u_mouse.value.x = e.pageX
-    uniforms.u_mouse.value.y = e.pageY
+    this.init()
   }
-  animate();
+
+  static loadTexture(url) {
+    return new Promise((resolve, reject) => {
+      const loader = new THREE.TextureLoader();
+      loader.load(url, (res) => {
+        resolve(res)
+      });
+    })
+  }
+
+
+  async init() {
+    this.container = document.getElementById('container');
+    this.camera = new THREE.Camera();
+    this.camera.position.z = 1;
+    this.scene = new THREE.Scene();
+    window.scene = this.scene
+    this.scene.add(this.camera)
+    const texture = await App.loadTexture("./uv.png")
+    texture.minFilter = THREE.NearestFilter;
+    texture.magFilter = THREE.NearestFilter;
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    const plane = new THREE.PlaneBufferGeometry(2, 2);
+    this.uniforms = {
+      u_time: { type: "f", value: 0 },
+      u_resolution: { type: "v3", value: new THREE.Vector3() },
+      u_mouse: { type: "v2", value: new THREE.Vector2() },
+      iChannel0: { type: "t", value: texture }
+    };
+
+    var material = new THREE.ShaderMaterial({
+      uniforms: this.uniforms,
+      vertexShader: vertexShader,//document.getElementById('vertexShader').textContent,
+      fragmentShader: fragmentShader//document.getElementById('fragmentShader').textContent
+    });
+
+    var mesh = new THREE.Mesh(plane, material);
+    scene.add(mesh);
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.autoClearColor = false;
+    this.container.appendChild(this.renderer.domElement);
+
+    this.onWindowResize();
+    window.addEventListener('resize', this.onWindowResize, false);
+    this.animate();
+  }
+
+  onWindowResize() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.uniforms.u_resolution.value.x = this.renderer.domElement.width;
+    this.uniforms.u_resolution.value.y = this.renderer.domElement.height;
+    this.uniforms.u_resolution.value.z = 1;
+  }
+
+  animate() {
+    requestAnimationFrame(this.animate);
+    this.render();
+  }
+
+  render() {
+    // this.uniforms.u_time.value += 0.05;
+    this.renderer.render(this.scene, this.camera);
+  }
 }
 
-function onWindowResize(event) {
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  uniforms.u_resolution.value.x = renderer.domElement.width;
-  uniforms.u_resolution.value.y = renderer.domElement.height;
-  uniforms.u_resolution.value.z = 1;
-  // console.error(uniforms.u_resolution)
-}
+new App()
 
-function animate() {
-  requestAnimationFrame(animate);
-  render();
-}
-
-function render() {
-  // uniforms.u_time.value += 0.05;
-  renderer.render(scene, camera);
-}
-
-
-init();
