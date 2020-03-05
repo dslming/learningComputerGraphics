01cf8bfee64792ffacd19041349b099c64c00a9f
dslming <1097364388@qq.com>
2020-03-05 20:17:20 +0800

    037-相机路径跟随

--- ThreejsLearning/037-相机路径跟随/App.js
+++ ThreejsLearning/037-相机路径跟随/App.js
@@ -2,7 +2,6 @@ import * as THREE from './lib/three.module.js'
 import { Stage } from './Stage.js'
 
 window.THREE = THREE
-
 var binormal = new THREE.Vector3();
 var normal = new THREE.Vector3();
 
@@ -16,75 +15,108 @@ class App {
   }
 
   addSpline() {
-    let basePoint = []
-    let r = 15
+    let points = []
+    let r = 20
     let z = 0
     for (let i = 0; i < 20; i += 1) {
       let x = r * Math.cos(i)
       let y = r * Math.sin(i)
-      basePoint.push(new THREE.Vector3(x, y, z))
-      r -= 0.5
+      points.push(new THREE.Vector3(x, y, z))
+      r -= 1
       z -= 1
     }
-
-    var path = new THREE.CatmullRomCurve3(basePoint);
-    lm.path = path
-    var geometry = new THREE.TubeGeometry(path, 200, 0.5, 8, false);
-    var material = new THREE.MeshLambertMaterial({ color: 0xff00ff, side: THREE.DoubleSide });
+    var path = new THREE.CatmullRomCurve3(points);
+    var geometry = new THREE.TubeGeometry(path, 200, 1, 20, false);
+    var material = new THREE.MeshLambertMaterial({ color: 0xff00ff, side: THREE.BackSide, wireframe: true });
     var curveObject = new THREE.Mesh(geometry, material);
-    var wireframeMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, opacity: 0.3, wireframe: true, transparent: true });
-    var wireframe = new THREE.Mesh(geometry, wireframeMaterial);
-    curveObject.add(wireframe);
-
     this.stage.scene.add(curveObject)
     this.curve = curveObject
+
+    // light
+    var light = new THREE.PointLight(0xffffff, 1, 50);
+    this.stage.scene.add(light);
+    var splineCamera = new THREE.PerspectiveCamera(85, 1, 0.1, 1000)
+    this.stage.scene.add(splineCamera);
+    var cameraHelper = new THREE.CameraHelper(splineCamera);
+    this.stage.scene.add(cameraHelper);
+
+
     this.stage.onUpdate(() => {
-      this.renderFollowCamera()
+      // splineCamera.lookAt(new THREE.Vector3(0, 0, -10))
+      // splineCamera.position.set(0, 0, 0)
+      // splineCamera.up.set(0, 1, 0)
+      // splineCamera.rotation.set
+      // splineCamera.rotation.setFromRotationMatrix(splineCamera.matrix, splineCamera.rotation.order);
+
+      this.renderFollowCamera(splineCamera, light, cameraHelper)
+
+      // this.ttt(splineCamera, light)
+      // this.stage.renderer.render(this.stage.scene, splineCamera);
     })
   }
 
+  ttt(splineCamera) {
+    var time = Date.now() * 0.5;
+    var looptime = 20 * 1000;
+    var t = (time % looptime) / looptime;
+
+    const tube = this.curve.geometry
+
+    var p1 = tube.parameters.path.getPointAt(t);
+    var p2 = tube.parameters.path.getPointAt((t + 0.005) % 1);
+    // console.error(pos);
+    splineCamera.lookAt(p2);
+    splineCamera.position.set(p1.x, p1.y, p1.z)
+    light.position.set(p2.x, p2.y, p2.z);
+  }
+
   // Animate the camera along the spline
-  renderFollowCamera() {
+  renderFollowCamera(splineCamera, light, cameraHelper) {
+    // splineCamera = this.stage.camera
+    // animate camera along spline
+    const tubeGeometry = this.curve.geometry
+    const params = {
+      scale: 1
+    }
     var time = Date.now();
     var looptime = 20 * 1000;
     var t = (time % looptime) / looptime;
-    const tube = this.curve.geometry
-    window.tt = tube
-    const splineCamera = this.stage.camera
-    const speed = 3
-    var pos = tube.parameters.path.getPointAt(t);
-    // console.error(pos);
 
-    pos.multiplyScalar(speed);
+    var pos = tubeGeometry.parameters.path.getPointAt(t);
+    pos.multiplyScalar(params.scale);
 
-    // interpolation 插补
-    var segments = tube.tangents.length;
+    // interpolation
+
+    var segments = tubeGeometry.tangents.length;
     var pickt = t * segments;
     var pick = Math.floor(pickt);
     var pickNext = (pick + 1) % segments;
 
-    binormal.subVectors(tube.binormals[pickNext], tube.binormals[pick]);
-    binormal.multiplyScalar(pickt - pick).add(tube.binormals[pick]);
+    binormal.subVectors(tubeGeometry.binormals[pickNext], tubeGeometry.binormals[pick]);
+    binormal.multiplyScalar(pickt - pick).add(tubeGeometry.binormals[pick]);
 
-    var dir = tube.parameters.path.getTangentAt(t);
+    var dir = tubeGeometry.parameters.path.getTangentAt(t);
+    var offset = 0;
+
     normal.copy(binormal).cross(dir);
 
-    // We move on a offset on its binormal
-    const offset = 5
+    // we move on a offset on its binormal
+
     pos.add(normal.clone().multiplyScalar(offset));
 
     splineCamera.position.copy(pos);
+    light.position.copy(pos);
+    // using arclength for stablization in look ahead
 
-    // Using arclength for stablization in look ahead.
-    var lookAt = tube.parameters.path.getPointAt((time + 30 / tube.parameters.path.getLength()) % 1).multiplyScalar(speed);
+    var lookAt = tubeGeometry.parameters.path.getPointAt((t + 30 / tubeGeometry.parameters.path.getLength()) % 1).multiplyScalar(params.scale);
 
-    // Camera Orientation 2 - up orientation via normal
-    // if (!lookAhead)
-    lookAt.copy(pos).add(dir);
+    // camera orientation 2 - up orientation via normal
+
+    if (!params.lookAhead) lookAt.copy(pos).add(dir);
     splineCamera.matrix.lookAt(splineCamera.position, lookAt, normal);
-    splineCamera.rotation.setFromRotationMatrix(splineCamera.matrix, splineCamera.rotation.order);
+    splineCamera.quaternion.setFromRotationMatrix(splineCamera.matrix);
+    cameraHelper.update()
 
-    // parent.rotation.y += (targetRotation - parent.rotation.y) * 0.05;
   }
 }
 
--- ThreejsLearning/038-样条曲线CatmullRom/note/note.md
+++ ThreejsLearning/038-样条曲线CatmullRom/note/note.md
@@ -31,7 +31,7 @@
 
 ![alt](005.png)
 
-#### 4、 CatmullRom 公式原理
+#### 4、 CatmullRom 几何原理
 
 [参考教程1](http://algorithmist.net/docs/catmullrom.pdf)
 [参考教程2](https://www.lucidarme.me/catmull-rom-splines/#)
@@ -39,25 +39,185 @@
 ![alt](007.png)
 要绘制曲线$P_iP_{i+1}$,需要额外两个辅助点, $P_{i-1}$和$P_{i+2}$。
 
-几乎所有的三次曲线构造都与 Hermite 曲线有些相似。考虑在两点之间拟合三次曲线的情况。除了通过两个点之外，还需要两个其他约束来定义曲线。假设在每个端点处指定了导数值，如下图所示:
+考虑在两点之间拟合三次曲线的情况。除了通过两个点之外，还需要两个其他约束来定义曲线。假设在每个端点处指定了导数值，如下图所示:
 ![alt](011.png)
 
-这种情况与 Hermite 曲线非常相似，只不过上面的曲线没有超出控制点。由于切线会影响曲线的形状，因此可以明确提供切线或从其他控制点推断出切线。
+由于切线会影响曲线的形状，因此可以明确提供切线或从其他控制点推断出切线。
 
-考虑单个曲线$P(t)$的推导，该曲线对上面的点$P_0$和$P_1$进行插值。
+#### 4、CatmullRom 公式推导
 
-假设如下所示指定了其他控制点$P_{-1}$和$P_2$。
-![alt](012.png)
+![alt](006.gif)
 
-$P(t)$是三次曲线，条件为t=0产生$P_0$，t=1产生$P_1$。
-辅助点$P_{-1}$和$P_2$用于通过隐式定义切线来调整曲线的形状:
-![alt](013.png)
+经过4个点($P_0$,$P_1$,$P_2$,$P_3$)的曲线方程为:
+$$
+B(t) = a + b t + c t^2 + d t^3 \tag{1}
+$$
 
-其中$α$在0到1之间。此定义使每个端点处的切线平行于相邻控制点之间的弦。曲线的一般方程为
-![alt](014.png)
+其切线方程为:
+$$
+C(t) = b + 2ct + 3 d t^2 \tag{2}
+$$
 
-可以从端点和切线规范提供的几何约束条件确定系数。该过程类似于Hermite插值的推导。
-![alt](015.png)
+考虑单个曲线$B(x)$的推导,这里:
 
-Since $P'(t)=3at2 +2bt+c$
-![alt](016.png)
+~~~math
+\begin{aligned}
+
+P_0 &= x_0 \\
+P_1 &= x_1 \\
+P_2 &= x_2 \\
+P_3 &= x_3
+\end{aligned}
+~~~
+
+将t=0和t=1分别带入公式(1)(2),可以得出:
+
+~~~math
+\begin{aligned}
+
+B(0) &= a = x_1 \\
+C(0) &= b \\
+B(1) &= a + b + c + d = x_2 \\
+C(1) &= b + 2c +3d
+
+\end{aligned}
+~~~
+
+令 $C(0)=t_0$,$C(1)=t_1$,写成矩阵形式为:
+$$
+\left[
+  \begin{matrix}
+  1 & 0 & 0 & 0  \\
+  0 & 1 & 0 & 0  \\
+  1 & 1 & 1 & 1  \\
+  0 & 1 & 2 & 3
+  \end{matrix}
+\right]
+*
+\left[
+  \begin{matrix}
+  a \\
+  b \\
+  c \\
+  d \\
+  \end{matrix}
+\right]
+= \left[
+  \begin{matrix}
+  x_1 \\
+  t_0 \\
+  x_2 \\
+  t_1 \\
+  \end{matrix}
+\right]
+$$
+
+根据矩阵乘法:
+
+~~~math
+\begin{aligned}
+
+A * X &= B   \\
+A^{-1} * A * X &= A^{-1} * B \\
+\end{aligned}
+~~~
+
+有:
+$$
+X = A^{-1}*B
+$$
+
+这里$A^{-1}$等于:
+$$
+\left[
+  \begin{matrix}
+  1 & 0 & 0 & 0  \\
+  0 & 1 & 0 & 0  \\
+  -3 & -2 & 3 & -1  \\
+  2 & 1 & -2 & 1
+  \end{matrix}
+\right]
+$$
+
+那么:
+$$
+\left[
+  \begin{matrix}
+  a \\
+  b \\
+  c \\
+  d \\
+  \end{matrix}
+\right]
+= \left[
+  \begin{matrix}
+  1 & 0 & 0 & 0  \\
+  0 & 1 & 0 & 0  \\
+  -3 & -2 & 3 & -1  \\
+  2 & 1 & -2 & 1
+  \end{matrix}
+\right]
+*
+\left[
+  \begin{matrix}
+  x_1 \\
+  t_0 \\
+  x_2 \\
+  t_1 \\
+  \end{matrix}
+\right]
+$$
+
+于是可以得出:
+~~~math
+\begin{aligned}
+
+a &= x_1 \\
+b &= t_0 \\
+c &= -3x_1 -2t_0 + 3x_2 + t1 \\
+d & = 2x_1 + t_0 -2x_2 + t_1
+
+\end{aligned}
+~~~
+
+#### 5、CatmullRom代码
+
+```js
+// 三次多项式
+class CubicPoly {
+  constructor() {
+    this.c0 = 0
+    this.c1 = 0
+    this.c2 = 0
+    this.c3 = 0
+  }
+
+  /*
+   * 计算三次多项式的系数
+   * Compute coefficients for a cubic polynomial
+   *   p(s) = c0 + c1*s + c2*s^2 + c3*s^3
+   * such that
+   *   p(0) = x0, p(1) = x1
+   *  and
+   *   p'(0) = t0, p'(1) = t1.
+   */
+  init(x0, x1, t0, t1) {
+    this.c0 = x0;
+    this.c1 = t0;
+    this.c2 = - 3 * x0 + 3 * x1 - 2 * t0 - t1;
+    this.c3 = 2 * x0 - 2 * x1 + t0 + t1;
+  }
+
+  initCatmullRom(x0, x1, x2, x3, tension) {
+    this.init(x1, x2, tension * (x2 - x0), tension * (x3 - x1));
+  }
+
+  calc(t) {
+    var t2 = t * t;
+    var t3 = t2 * t;
+    return this.c0 + this.c1 * t + this.c2 * t2 + this.c3 * t3;
+  }
+}
+
+```
+<全文结束>
